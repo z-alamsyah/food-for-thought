37dd25d688d384fcbf056146f5bfb67b91288414
zico <z.alamsyah1108@gmail.com>
2025-05-05 23:27:02 +0700

    Add Get Sign URL Function

--- cloud_compute/gcp/cloud-storage.js
+++ cloud_compute/gcp/cloud-storage.js
@@ -1,20 +1,24 @@
 import { Storage } from "@google-cloud/storage";
 
 export default class CloudStorage {
-   async uploadFile() {
-      const storage = new Storage({
-        keyFilename: "key.json",
-      });
-  
-      // The ID of your GCS bucket
-      const bucketName = "my-bucket-1357";
-  
+  // The ID of your GCS bucket
+  bucketName = "my-bucket-1357";
+  //initialize storage
+  storage = Storage;
+
+  constructor(){
+    this.storage = new Storage({
+      keyFilename: "key.json",
+    });
+  }
+
+   async uploadFile(file, fileTarget, folderTarget) {
       // The path to your file to upload
-      const filePath = 'test-doc-csv.csv';
+      const filePath = file;
   
       // The new ID for your GCS file
       const date = new Date();
-      const destFileName = 'docs/my-file-csv-' + date.toISOString();
+      const destFileName = `${folderTarget}/${fileTarget}-` + date.toISOString();
   
       //upload doc
       const options = {
@@ -22,8 +26,25 @@ export default class CloudStorage {
         preconditionOpts: {ifGenerationMatch: 0},
       };
   
-      await storage.bucket(bucketName).upload(filePath, options);
-      console.log(`${filePath} uploaded to ${bucketName}`);
+      const resp = await this.storage.bucket(this.bucketName).upload(filePath, options);
+      console.log(`${filePath} uploaded to ${this.bucketName}`);
+      
+      return destFileName;
+    }
+
+    async getSignedUrl(filePath, expiresInSeconds = 3600) {
+      const options = {
+        version: 'v4',
+        action: 'read',
+        expires: Date.now() + expiresInSeconds * 1000, // Default: 1 hour
+      };
+  
+      const [url] = await this.storage
+        .bucket(this.bucketName)
+        .file(filePath)
+        .getSignedUrl(options);
+  
+      return url;
     }
 }
 
--- cloud_compute/gcp/index.js
+++ cloud_compute/gcp/index.js
@@ -1,4 +1,13 @@
 import CloudStorage from "./cloud-storage.js";
 
 const storage = new CloudStorage();
-await storage.uploadFile();
+
+
+// upload images
+const objFile = await storage.uploadFile('dart.jpeg', 'my-image', 'images');
+const signURL = await storage.getSignedUrl(objFile, 120);
+
+
+//For Access signURL, you can use <img src="" />
+//<img src="https://storage.googleapis.com/my-bucket-1357/images/my-image-...signed-part..." />
+console.log('Access URL:', signURL);
